aceddac860cbab833b4cdb7dfc96c69fc64cb1e3
Dapunta <dapuntaxd@gmail.com>
2025-04-14 21:12:08 +0700

    update result 1

--- sql/indexing/hapus_index.sql
+++ sql/indexing/hapus_index.sql
@@ -1,3 +1,7 @@
+-- bawaan query
+DROP INDEX IF EXISTS index_order_details_pk;
+DROP INDEX IF EXISTS index_order_pk;
+
 -- query berat 1
 DROP INDEX IF EXISTS idx_orders_order_date;
 DROP INDEX IF EXISTS idx_order_details_unit_price;
